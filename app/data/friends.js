627dc4e1bf70583f09b6d3133cbd6728c1728f4f
Your Name <you@example.com>
2019-06-24 00:28:41 -0700

    friends list

---
+++ app/data/friends.js
@@ -0,0 +1,74 @@
+var friends = [
+    {
+        name: "Nikki",
+        photo: "https://cdn.pixabay.com/photo/2016/04/01/08/43/cartoon-1298902_960_720.png",
+        scores:[
+            "4",
+            "5",
+            "3",
+            "5",
+            "2",
+            "3",
+            "2",
+            "4",
+            "4",
+            "2"
+
+        ]
+
+    },
+    {
+        name: "Shawn",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfK-MRS_WfCulpXZhOatZkat0IU-c7l7Zc-g0GJ4ZdLLLoTOb9",
+        scores:[
+            "2",
+            "4",
+            "3",
+            "5",
+            "3",
+            "5",
+            "3",
+            "3",
+            "4",
+            "3"
+
+        ]
+    },
+    {
+        name: "Tom",
+        photo: "https://cdn.pixabay.com/photo/2016/04/01/00/07/boy-1298009_960_720.png",
+        scores:[
+            "4",
+            "4",
+            "5",
+            "3",
+            "3",
+            "2",
+            "3",
+            "4",
+            "2",
+            "5"
+
+        ]
+
+    },
+    {
+        name: "Shanna",
+        photo: "https://cdn.pixabay.com/photo/2016/03/31/22/56/blonde-1297289_960_720.png",
+        scores:[
+            "5",
+            "4",
+            "2",
+            "3",
+            "2",
+            "3",
+            "1",
+            "5",
+            "3",
+            "4"
+
+        ]
+
+    }
+
+]
