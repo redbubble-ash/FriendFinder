ba9eabc2ea4091b91b44a37f53b450d1cf26fe2b
Your Name <you@example.com>
2019-06-25 02:36:02 -0700

    reset totalscoredifference to zero when start to compare with a new friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,24 +12,30 @@ module.exports = (router) => {
     router.post("/api/friends", (req, res) => {
 
         var friendMatch = {
-            name : "",
-            photo : "",
-            scoreDifference : Infinity
+            name: "",
+            photo: "",
+            scoreDifference: Infinity
         }
 
         var userData = req.body;
         var userScore = req.body.scores;
-        var totalScoreDifference = 0;
+        var totalScoreDifference ;
 
 
-        for (i = 0; i < friends.length; i++) {
+        for (var i = 0; i < friends.length; i++) {
 
             var selectedFriend = friends[i];
+            
+            //reset to zero when start to compare with a new friend
+            totalScoreDifference = 0;
 
-            for (j = 0; j < selectedFriend.scores[j]; j++) {
+            console.log(selectedFriend.name);
+
+            for (var j = 0; j < selectedFriend.scores.length; j++) {
 
                 var scoreToCompare = selectedFriend.scores[j];
-                totalScoreDifference += Math.abs(parseInt(scoreToCompare) - parseInt(userScore));
+                var currentUserScore = userScore[j];
+                totalScoreDifference += Math.abs(parseInt(scoreToCompare) - parseInt(currentUserScore));
 
             }
 
@@ -37,16 +43,21 @@ module.exports = (router) => {
             if (totalScoreDifference <= friendMatch.scoreDifference) {
 
                 friendMatch.scoreDifference = totalScoreDifference;
-                friendMatch.name = friends[i].name;
-                friendMatch.photo = friends[i].photo;
+                friendMatch.name = selectedFriend.name;
+                friendMatch.photo = selectedFriend.photo;
 
             }
 
         }
 
+        // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
+        // the database will always return that the user is the user's best friend).
+
         friends.push(userData);
 
+        // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
         res.json(friendMatch)
+        console.log("Best Match is " + JSON.stringify(friendMatch.scoreDifference) + JSON.stringify(friendMatch.name) )
 
     })
 
